75e16656adeda8df4ccff0f82dfb4ea8fd60e3fb
Lokesh <lokeshloks815@gmail.com>
2025-01-31 19:10:34 -0600

    Added Lokesh_As1_Task_1.cs and Lokesh_As1_Task_2.cs

---
+++ Lokesh_As1_Task_1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Task_1
+    {
+        static void Main(string[] args)
+        {
+            
+            int TorontoTOCalgary = 1350*2;
+            int TorontoTOVancouver = 1500*2;
+            int TorontoToMontreal = 575*2;
+
+            
+
+            // Total number of trips to Calgary
+            Console.Write("Enter the total number of trips did Carlo took from Toronto to Calgary: ");
+            // Reading input as string and converting into integer
+            int TotalCalgaryTrip = int.Parse(Console.ReadLine());
+            if (TotalCalgaryTrip < 1)
+            {
+                Console.WriteLine("Please enter a valid number");
+                return;
+            }
+            // Formula for calculating trip cost from Toronto to Calgary
+            double TotalSpentForCalgary = (TorontoTOCalgary * TotalCalgaryTrip);
+            //Printing the total expense for Caralo from Toronto to Calgary
+            Console.WriteLine("The total expense for Caralo from Toronto to Calgary is:" + "$" + TotalSpentForCalgary);
+
+
+            // Total number of trips to Vancouver
+            Console.Write("Enter the total number of trips did Carlo took from Toronto to Vancouver:");
+            // Reading input as string and converting into integer
+            int TotalVancouverTrip = int.Parse(Console.ReadLine());
+            if  (TotalVancouverTrip < 1)
+                {
+                Console.WriteLine("Please enter a valid number");
+                return;
+
+            }
+            // Formula for calculating trip cost from Toronto to  Vancouver
+            double TotalSpentForVancover = (TorontoTOVancouver * TotalVancouverTrip);
+            //Printing the total expense for Caralo from Toronto to Vancouver
+            Console.WriteLine("The total expense for Caralo from Toronto to Vancouver is:" + "$" + TotalSpentForVancover);
+
+
+            // Total number of trips to Vancouver
+            Console.Write("Enter the total number of trips did Carlo took from Toronto to Montreal:");
+            // Reading input as string and converting into integer
+            int TotalMontrealTrip = int.Parse(Console.ReadLine());
+            if (TotalMontrealTrip < 1)
+            {
+                Console.WriteLine("Please enter a valid number");
+                return;
+            }
+            // Formula for calculating trip cost from Toronto to Montreal
+            double TotalSpentForMontreal = (TorontoToMontreal * TotalMontrealTrip);
+            //Printing the total expense for Caralo from Toronto to Montreal
+            Console.WriteLine("The total expense for Caralo from Toronto to Montreal is:" + "$" + TotalSpentForMontreal);
+
+
+            Console.WriteLine("\n ------ Summary for Caralo's trip-------");
+
+
+
+            //Formula for Calculating the overall trip cost
+            double TotalTripCost = (TotalSpentForCalgary + TotalSpentForVancover + TotalSpentForMontreal);
+            // Printing the overall trip cost
+            Console.WriteLine("\n Money spent by Caralo for the overall trip is:" + "$" + TotalTripCost);
+
+
+            //Total number of trips by Carlo
+            int total_trips = (TotalCalgaryTrip + TotalVancouverTrip + TotalMontrealTrip );
+            Console.WriteLine("Total number of trips by Caralo is:" + total_trips + "trips" );
+
+
+            //Explicit Casting - Conversion of double to float - Example to understand the concept
+
+            //double doubleValue = value;
+            //float floatValue = (float)doubleValue; // Explicit conversion
+            //Console.WriteLine(floatValue);
+
+            // Average overall trip cost 
+            double AverageCost = (TotalTripCost / total_trips);
+            float AverageTripCost = (float)AverageCost;
+            Console.WriteLine("Average cost of a trip is:" + "$" + AverageTripCost);
+
+
+        }
+    }
+}
---
+++ Lokesh_As1_Task_2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_task2
+{
+    internal class Task_2
+    {
+        static void Main(string[] args)
+        {
+            //Initializing ticket price for Purple, Green and Blue
+            int PurpleTicketPrice = 50;
+            int GreenTicketPrice = 80;
+            int BlueTicketPrice = 100;
+
+            //Initializing ticket variable     
+            int PurpleTicket,GreenTicket,BlueTicket;
+
+            Console.WriteLine("Joe's Raptors Game");
+            
+                // Getting input for purple ticket
+                Console.Write("Enter Purple ticket:");
+                PurpleTicket = int.Parse(Console.ReadLine());
+                // Making to show error message, if user gives ticket lesser than 1
+                if (PurpleTicket < 1)
+                {
+                    Console.WriteLine("Enter a valid number");
+                    return;
+                }
+
+                // Printing amount spent for purple ticket           
+                Console.WriteLine("Amount spent for Purple ticket: $" + PurpleTicketPrice * PurpleTicket);
+            
+
+            // Getting input from user for Green ticket
+            Console.Write("Enter Green ticket:");
+            GreenTicket = int.Parse(Console.ReadLine());
+            // Making to show error message, if user gives ticket lesser than 1
+            if (GreenTicket < 1)
+            {
+                Console.WriteLine("Enter a valid number");
+                return;
+            }
+            // Printing amount spent for Green ticket
+            Console.WriteLine("Amount spent for Green ticket: $" + GreenTicketPrice * GreenTicket);
+
+            // Getting input from user for Blue ticket
+            Console.Write("Enter Blue ticket:");
+            BlueTicket = int.Parse(Console.ReadLine());
+
+            //Making to show error message, if user gives ticket lesser than 1
+            if (BlueTicket < 1)
+            {
+                Console.WriteLine("Enter a valid number");
+                return;
+            }   
+            // Printing amount spent for Blue ticket
+            Console.WriteLine("Amount spent for Blue ticket: $" + BlueTicketPrice * BlueTicket);
+
+            // Total tickets
+            int totalGames = PurpleTicket + GreenTicket + BlueTicket;
+            // Total amount spent on all tickets
+            int totalAmount = (PurpleTicketPrice * PurpleTicket) + (GreenTicketPrice * GreenTicket) + (BlueTicketPrice * BlueTicket);
+            // Average cost 
+            int average_cost =  totalAmount/totalGames;
+            double average_cost_ticket = (double)average_cost;
+
+            Console.WriteLine("\n -------Joe's Game Summary--------"); 
+
+            // Printing Total games attended by Joe, Total amount spent, and Average amount spent
+            Console.WriteLine("\nTotal Games: " + totalGames);
+            Console.WriteLine("Total amount spent by Joe is: $" + totalAmount);
+            Console.WriteLine("Average amount spent by Joe is: $" + average_cost);
+
+            
+        }
+    }
+}   
